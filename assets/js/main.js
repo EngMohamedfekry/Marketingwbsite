655ecdbfaf92cdf941b06af19e2f52e97e27880d
Abdulrahman-Nasser <aboudnaser20@gmail.com>
2023-08-30 17:35:36 +0300

    advertise box added success

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,22 +30,25 @@ $(function () {
 
 // owl carousel
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
         },
-        600:{
-            items:3,
-            nav:false
+        600: {
+            items: 3,
+            nav: false
         },
-        1000:{
-            items:1,
-            nav:true,
-            loop:true
+        1000: {
+            items: 1,
+            nav: true,
+            loop: true
         }
     }
 })
